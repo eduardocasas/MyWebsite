7773eec42cfed488d767d6140e08763b580ccb66
eduardocasas <eduardo.casas.lopez@gmail.com>
2014-05-15 19:18:16 +0200

    fix bower packages. add code button to tinymce

--- www/symfony/web/bundles/ecldefault/js/pc/backoffice/init_tinymce.js
+++ www/symfony/web/bundles/ecldefault/js/pc/backoffice/init_tinymce.js
@@ -2,8 +2,8 @@ tinymce.init({
     selector : "#article_form_article_extend_content",
     menubar:false,
     statusbar: false,
-    plugins: ["fullscreen image link textcolor"],
-    toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | removeformat | bullist numlist | link image | fullscreen",
+    plugins: ["fullscreen image link textcolor code"],
+    toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | removeformat | bullist numlist | link image | fullscreen code",
     image_advtab: true,
     setup : function(ed) { ed.on('init',function(){this.getDoc().body.style.fontSize = '0.85em';});}
 });
