2fa18a37984cd75d07f5e6ab0a5cfec6d6b99c21
eduardocasas <eduardo.casas.lopez@gmail.com>
2015-08-08 19:38:55 +0200

    remove browscap dependency

--- www/symfony/src/ECL/DefaultBundle/Service/Browser.php
+++ www/symfony/src/ECL/DefaultBundle/Service/Browser.php
@@ -1,36 +1,22 @@
 <?php
 namespace ECL\DefaultBundle\Service;
 
-use phpbrowscap\Browscap;
-
 class Browser
 {
 
-    private $Broser;
     
     public function __construct($service_container)
     {
-        $this->Broser = (new Browscap($service_container->getParameter("kernel.cache_dir")))->getBrowser();
     }
     
     public function getFolder()
     {
-        return ($this->isMobile()) ?  'mobile' : 'pc';
+        return 'pc';
     }
     
     public function hasWebKitRenderingEngine()
     {
-        return $this->getRenderingEngine() == 'WebKit';
+        return true;
     }
     
-    private function getRenderingEngine()
-    {
-        return $this->Broser->RenderingEngine_Name;
-    }
-    
-    private function isMobile()
-    {
-        return $this->Broser->isMobileDevice;
-    }
-
 }
