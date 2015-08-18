c794864906627d713379cfb30fe48c633af3ac94
eduardocasas <eduardo.casas.lopez@gmail.com>
2015-08-18 10:43:10 +0200

    refactor syntaxhighlighter and add responsive design

--- www/symfony/web/bundles/ecl/js/base.js
+++ www/symfony/web/bundles/ecl/js/base.js
@@ -1,44 +1,23 @@
-$(document).ready
-(
-    function()
-    {
+$(document).ready(function() {
         
-        /* ----- Header ----- */
-        
-        var main_header       = $('#main_header_wrapper'),
-            scroll_top_button = $('#scroll_top_button');
-    
-        main_header.hover
-        (
-            function()
-            {
-                main_header.removeClass('onscroll');
-            },
-            function()
-            {
-                if ($(document).scrollTop() != 0) {
-                    main_header.addClass('onscroll');
-                }
-            }
-        );
-        $(window).scroll
-        (
-            function()
-            {
-                main_header.add(scroll_top_button).addClass('onscroll');
-                if ($(document).scrollTop() == 0) {
-                    main_header.add(scroll_top_button).removeClass('onscroll');
-                }
-            }
-        );
-        scroll_top_button.click
-        (
-            function(event)
-            {
-                event.preventDefault();
-                $('html, body').animate({ scrollTop: 0 }, 600);
-            }
-        );
+    /* ----- Header ----- */
 
-    }
-);
+    var headerWrapper = $('#headerWrapper_wrapper');
+
+    headerWrapper.hover(
+        function() { headerWrapper.removeClass('onscroll') },
+        function() {
+            if ($(document).scrollTop() != 0) {
+                headerWrapper.addClass('onscroll');
+            }
+        }
+    );
+    $(window).scroll(function() {
+        headerWrapper.addClass('onscroll');
+        if ($(document).scrollTop() == 0) {
+            headerWrapper.removeClass('onscroll');
+        }
+    });
+    $('#display_sidebar_button').click(function() { $('#default_sidebar_wrapper').toggle() });
+
+});
