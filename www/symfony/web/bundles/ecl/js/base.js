401cad855e6ab30f5f299a94090c5ca5dd58c223
eduardocasas <eduardo.casas.lopez@gmail.com>
2016-03-13 10:43:09 +0100

    hide header when scrolldown (1)

--- www/symfony/web/bundles/ecl/js/base.js
+++ www/symfony/web/bundles/ecl/js/base.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
-        
-    /* ----- Header ----- */
-
+    
+    $('#display_sidebar_button').click(function() { $('#default_sidebar_wrapper, #nav').toggle(); });
+    
+    /* -------------- Hide/Show header when scrolling -------------- */
+    
+    var mobileView = $('#default_sidebar_wrapper').css('position') == 'fixed';
+    $(window).on('resize', function(){
+        mobileView = $('#default_sidebar_wrapper').css('position') == 'fixed';
+        if (!mobileView) {
+            headerWrapper.show();   
+        }
+    });
     var headerWrapper = $('#main_header_wrapper');
     var headerWrapperHeight = headerWrapper.height();
     var lastScrollTop = 0;
 
     $(window).scroll(function() {
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop){
-            if ($(document).scrollTop() < headerWrapperHeight) {
-                headerWrapper.slideDown();
+        if (mobileView) {
+            var st = $(this).scrollTop();
+            if (st > lastScrollTop){
+                if ($(document).scrollTop() < headerWrapperHeight) {
+                    headerWrapper.slideDown();
+                } else if ($('#default_sidebar_wrapper').css('display') == 'none') {
+                    headerWrapper.hide();
+                }
             } else {
-                headerWrapper.hide();
+              headerWrapper.slideDown();
             }
-        } else {
-          headerWrapper.slideDown();
+            lastScrollTop = st;        
         }
-        lastScrollTop = st;        
-    });
-    $('#display_sidebar_button').click(function() { $('#default_sidebar_wrapper, #nav').toggle(); });
+    });    
+    
 
 });
