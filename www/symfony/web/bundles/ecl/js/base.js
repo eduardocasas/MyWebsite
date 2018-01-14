5662bd7f20fdb84c93ad748d54b4268e0fe98da6
eduardocasas <eduardo.casas.lopez@gmail.com>
2018-01-14 08:58:58 +0100

    improved responsive design

--- www/symfony/web/bundles/ecl/js/base.js
+++ www/symfony/web/bundles/ecl/js/base.js
@@ -1,31 +1,74 @@
 $(document).ready(function() {
     
-    $('#display_sidebar_button').click(function() { $('#default_sidebar_wrapper, #nav').toggle(); });
+    /* -------------- Switch Language -------------- */
+    
+    $('#language').change(function() {
+        window.location.href = $(':selected', $(this)).data('url');
+    });
+    
+    /* ---------------- Responsive Design ---------------- */
+    
+    var isMobileView = $('#sidebar').css('display') == 'none';
+    var header = $('#header');
+    var footer = $('#footer');
+    var mobileSidebar = $('#nav');
+    var mobileSidebarIsVisible = false;
+    function isScrolledIntoView() {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = footer.offset().top;
+        var elemBottom = elemTop + footer.height();
+        return (elemBottom <= docViewBottom) && (elemTop >= docViewTop);
+    }        
+    function setMobileSidebarSettings() {
+        if (isScrolledIntoView()) {
+            mobileSidebar.css('max-height', $(window).height()-header.height()-footer.height());
+        } else {
+            mobileSidebar.css('max-height', $(window).height()-header.height());
+        }
+    }
+    if (isMobileView) {
+        setMobileSidebarSettings();
+    }
+    $('#display_sidebar_button').click(function() { 
+        if (mobileSidebarIsVisible) {
+            mobileSidebar.animate({ marginLeft: '-='+(mobileSidebar.width()+1)+'px' });
+            $('#disable_background_on_sidebar').animate({ opacity: '-=0.5' }).promise().done(function () {
+                $('#disable_background_on_sidebar').hide();
+            });                        
+            mobileSidebarIsVisible = false;
+        } else {
+            $('#disable_background_on_sidebar').show();
+            mobileSidebar.animate({ marginLeft: '+='+(mobileSidebar.width()+1)+'px' });   
+            $('#disable_background_on_sidebar').animate({ opacity: '+=0.5' });
+            mobileSidebarIsVisible = true;
+        }
+        
+    });
+    $(window).on('resize', function(){
+        isMobileView = $('#sidebar').css('display') == 'none';
+        if (!isMobileView) {
+            header.show();   
+        } else {
+            setMobileSidebarSettings();
+        }
+    });    
     
     /* -------------- Hide/Show header when scrolling -------------- */
     
-    var mobileView = $('#default_sidebar_wrapper').css('position') == 'fixed';
-    $(window).on('resize', function(){
-        mobileView = $('#default_sidebar_wrapper').css('position') == 'fixed';
-        if (!mobileView) {
-            headerWrapper.show();   
-        }
-    });
-    var headerWrapper = $('#main_header_wrapper');
-    var headerWrapperHeight = headerWrapper.height();
+    
     var lastScrollTop = 0;
-
     $(window).scroll(function() {
-        if (mobileView) {
+        if (isMobileView) {
             var st = $(this).scrollTop();
             if (st > lastScrollTop){
-                if ($(document).scrollTop() < headerWrapperHeight) {
-                    headerWrapper.slideDown();
-                } else if ($('#default_sidebar_wrapper').css('display') == 'none') {
-                    headerWrapper.hide();
+                if ($(document).scrollTop() < header.height()) {
+                    header.slideDown();
+                } else if (!mobileSidebarIsVisible) {
+                    header.hide();
                 }
             } else {
-              headerWrapper.slideDown();
+              header.slideDown();
             }
             lastScrollTop = st;        
         }
